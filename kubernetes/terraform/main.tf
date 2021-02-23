c06a542fdb732e865d019e27b2671fadbdf310a9
Albert <palmin.albert@yandex.ru>
2021-02-23 18:50:43 +0300

    add files for kubernetes-4 homework

--- kubernetes/terraform/main.tf
+++ kubernetes/terraform/main.tf
@@ -1,5 +1,4 @@
 provider "yandex" {
-  service_account_key_file = var.service_account_key_file
   folder_id                = var.folder_id
 }
 
@@ -12,13 +11,13 @@ resource "yandex_kms_symmetric_key" "key-a" {
 resource "yandex_kubernetes_cluster" "reddit_cluster_resource_name" {
   name        = "name"
   description = "description"
-
   network_id = var.network_id
 
   master {
-    version = "1.18"
+    version = "1.15"
     zonal {
       subnet_id = var.subnet_id
+      zone = "ru-central1-a"
     }
 
     public_ip = true
@@ -33,7 +32,7 @@ resource "yandex_kubernetes_cluster" "reddit_cluster_resource_name" {
   node_service_account_id = var.service_account_key_id
 
 
-  release_channel = "RAPID"
+  release_channel = "STABLE"
   network_policy_provider = "CALICO"
 
   kms_provider {
@@ -45,7 +44,7 @@ resource "yandex_kubernetes_node_group" "my_node_group" {
   cluster_id  = "${yandex_kubernetes_cluster.reddit_cluster_resource_name.id}"
   name        = "name"
   description = "description"
-  version     = "1.18"
+  version     = "1.15"
 
 
   instance_template {
@@ -53,13 +52,13 @@ resource "yandex_kubernetes_node_group" "my_node_group" {
     nat         = true
 
     resources {
-      memory = 2
-      cores  = 2
+      memory = 8
+      cores  = 4
     }
 
     boot_disk {
       type = "network-hdd"
-      size = 64
+      size = 150
     }
 
     scheduling_policy {
@@ -69,7 +68,7 @@ resource "yandex_kubernetes_node_group" "my_node_group" {
 
   scale_policy {
     fixed_scale {
-      size = 3
+      size = 2
     }
   }
 
